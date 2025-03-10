6a6b98e7e286bd59d7060d1099787b80874da987
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-03-10 18:59:49 -0500

    add_css

--- parcial1/src/components/UserItem/UserItem.jsx
+++ parcial1/src/components/UserItem/UserItem.jsx
@@ -1,6 +1,6 @@
 const UserItem = ({name, email, role}) => {
     return(
-        <div className="user">
+        <div className="user-data">
             <p>{name}</p>
             <p>{email}</p>
             <p>{role}</p>
